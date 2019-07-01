2d914a191560e5bebb9cdeb95b759ab1f00c1a44
andrew.wang <qwwangcd@cn.ibm.com>
2019-07-01 17:41:08 +0800

    Initial Commit

---
+++ alignDev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alignDev
+//
+//  Created by 汪奇伟 on 2019/7/1.
+//  Copyright © 2019 汪奇伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
